9f3ee3440fd48e5b6764a1237c70ab5959447ac7
Damjan15 <stamenovdamjan5@gmail.com>
2025-03-25 22:37:40 +0100

    feat: responsive navbar added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World from Leno!");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get hamburger button and mobile menu items
+  const toggleButton = document.querySelector(".navbar__mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar__mobile-menu-items");
+
+  // Add click event listener to toggle menu visibility
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});

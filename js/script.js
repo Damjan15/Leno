08b2ee31109d9a3a23ed40aa344a4bc976e477d5
Damjan15 <stamenovdamjan5@gmail.com>
2025-03-26 10:17:16 +0100

    feat: modal added

--- js/script.js
+++ js/script.js
@@ -7,6 +7,37 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleButton.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+
+  // === Video modal functionality ===
+  const modal = document.getElementById("videoModal");
+  const videoButton = document.querySelector(".preview__video-button");
+  const closeButton = document.querySelector(".modal__close-button");
+
+  // Open the modal when the button is clicked
+  videoButton.addEventListener("click", function () {
+    modal.style.display = "block";
+
+    document.getElementById("videoPlayer").src =
+      "https://www.youtube.com/embed/8sXRyHI3bLw'";
+  });
+
+  // Close the modal when the button is clicked
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+
+    // Pause the video when the modal is closed
+    document.getElementById("videoPlayer").src = "";
+  });
+
+  // Close the modal when clicking outside of it
+  window.addEventListener("click", function (e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+
+      // Pause the video when the modal is closed
+      document.getElementById("videoPlayer").src = "";
+    }
+  });
 });
 
 window.addEventListener("scroll", function () {
